3b22bbbcce593511b1a97345d652244e84aece98
Luca Guidi <guidi.luca@gmail.com>
2011-04-08 18:34:19 +0200

    When compress stylesheets, append a timestamp for all the images used.
    Example:
      div.logo{ background: url(/images/people/logo.png?1286374077) top left; }

--- lib/assets_packager/compressors/stylesheet.rb
+++ lib/assets_packager/compressors/stylesheet.rb
@@ -14,13 +14,22 @@ module AssetsPackager
 
         # Thanks to Scott Becker
         def self._compress_css(content) #:nodoc:
-          content.gsub!(/\s+/, " ")           # collapse space
-          content.gsub!(/\/\*(.*?)\*\/ /, "") # remove comments - caution, might want to remove this if using css hacks
-          content.gsub!(/\} /, "}\n")         # add line breaks
-          content.gsub!(/\n$/, "")            # remove last break
-          content.gsub!(/ \{ /, " {")         # trim inside brackets
-          content.gsub!(/; \}/, "}")          # trim inside brackets
+          content.gsub!(/\s+/, " ")                                                           # collapse space
+          content.gsub!(/\/\*(.*?)\*\/ /, "")                                                 # remove comments - caution, might want to remove this if using css hacks
+          content.gsub!(/\} /, "}\n")                                                         # add line breaks
+          content.gsub!(/\n$/, "")                                                            # remove last break
+          content.gsub!(/ \{ /, " {")                                                         # trim inside brackets
+          content.gsub!(/; \}/, "}")                                                          # trim inside brackets
+          content.gsub!(/url\((.*)\.(gif|jpg|png)\)/) { "url(#{_timestamp("#{$1}.#{$2}")})" } # append a timestamp like: /images/logo.png?1286374077
           content
+        end
+
+        def self._timestamp(path) #:nodoc:
+          if ::File.exist?( expanded_path = ::File.expand_path(::File.join(AssetsPackager::Configuration.root_path, path)) )
+            path << "?#{::File.mtime(expanded_path).to_i}"
+          end
+
+          path
         end
     end
   end
